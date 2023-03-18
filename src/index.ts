f124b312616a61966d33147ee0b3398c75a97a18
etpotato <mareev.pv@gmail.com>
2023-03-18 12:21:05 +0500

    add tg-api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('dd')
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
+
+const bot = new Telegraf(process.env.BOT_TOKEN || '')
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
